9feeaa27e3368564a96111948bdbf3d14e1231f3
Jamie Murphy <hello@itsjamie.dev>
2023-05-16 01:48:13 -0700

    mastodon: Move stuff to mastodo file

--- main.go
+++ main.go
@@ -1,57 +1,19 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"math/rand"
 	"sync"
 	"time"
 
 	"github.com/go-co-op/gocron"
-	"github.com/vartanbeno/go-reddit/v2/reddit"
 )
 
-func Fetch() *reddit.Post {
-	client := reddit.DefaultClient()
-	posts, _, err := client.Subreddit.HotPosts(context.Background(), "capybara", &reddit.ListOptions{
-		Limit: 150, After: "", Before: "",
-	})
-
-	if err != nil {
-		panic("handle this later lmao")
-	}
-
-	posts = Filter(posts, func(p *reddit.Post) bool {
-		// TODO: Save image links so we don't have duplicates
-		return !p.IsSelfPost && p.UpvoteRatio >= 0.9
-	})
-
-	var scores []int
-	for _, s := range posts {
-		scores = append(scores, s.Score)
-	}
-	posts = Filter(posts, func(p *reddit.Post) bool {
-		best_score := Median(scores)
-		return p.Score >= best_score
-	})
-
-	best_post := posts[rand.Intn(len(posts))]
-
-	fmt.Printf(best_post.URL)
-
-	return best_post
-}
-
 func main() {
-	minute_schedule := flag.Int("schedule", 60, "How often to post (in minutes)")
-	flag.Parse()
-
+	config := LoadConfig()
 	scheduler := gocron.NewScheduler(time.UTC)
 	var wg sync.WaitGroup
 
-	job, err := scheduler.Every(*minute_schedule).Minutes().Do(func() {
-		Fetch()
+	job, err := scheduler.Every(config.Schedule).Minutes().Do(func() {
+		UploadMedia(config)
 	})
 	if err != nil {
 		panic("Unable to start scheduler")
---
+++ mastodon.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/mattn/go-mastodon"
+	"github.com/vartanbeno/go-reddit/v2/reddit"
+)
+
+type MediaAttachments struct {
+	ImageSizeLimit     int      `json:"image_size_limit"`
+	ImageMatrixLimit   int      `json:"image_matrix_limit"`
+	SupportedMimeTypes []string `json:"supported_mime_types"`
+}
+
+type Configuration struct {
+	Attachments MediaAttachments `json:"media_attachments"`
+}
+
+type Config struct {
+	Config Configuration `json:"configuration"`
+}
+
+func GetMastodonClientWithLimits(GlobalConfig *GlobalConfig) (*mastodon.Client, Config) {
+	c := mastodon.NewClient(&mastodon.Config{
+		Server:       GlobalConfig.APIBaseURL,
+		ClientID:     GlobalConfig.ClientID,
+		ClientSecret: GlobalConfig.ClientSecret,
+		AccessToken:  GlobalConfig.AccessToken,
+	})
+
+	var data *Config
+	url, _ := url.Parse(c.Config.Server)
+	url.Path = "api/v2/instance"
+	req, err := http.Get(url.String())
+	if err != nil {
+		log.Fatal("Unable to make request to Mastodon server, please check that the URL is correct", ": ", err)
+	}
+	body, _ := io.ReadAll(req.Body)
+	req.Body.Close()
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal("Unable to parse server response on endpoint /api/v2/instance", ": ", err)
+	}
+
+	return c, *data
+}
+
+func Fetch(GlobalConfig *GlobalConfig) *reddit.Post {
+	client := reddit.DefaultClient()
+	posts, _, err := client.Subreddit.HotPosts(context.Background(), GlobalConfig.Subreddit, &reddit.ListOptions{
+		Limit: 150, After: "", Before: "",
+	})
+
+	if err != nil {
+		panic("handle this later lmao")
+	}
+
+	posts = Filter(posts, func(p *reddit.Post) bool {
+		// TODO: Save image links so we don't have duplicates
+		return !p.IsSelfPost && p.UpvoteRatio >= 0.9
+	})
+
+	var scores []int
+	for _, s := range posts {
+		scores = append(scores, s.Score)
+	}
+	posts = Filter(posts, func(p *reddit.Post) bool {
+		best_score := Median(scores)
+		return p.Score >= best_score
+	})
+
+	best_post := posts[rand.Intn(len(posts))]
+
+	fmt.Printf(best_post.URL)
+
+	return best_post
+}
+
+func ValidateMedia(c Config, URL string) bool {
+	req, err := http.Head(URL)
+	if err != nil {
+		log.Fatal("Unable to make HEAD request to endpoint ", URL, ": ", err)
+	}
+	str, _ := strconv.Atoi(req.Header.Get("Content-Length"))
+	if str <= c.Config.Attachments.ImageSizeLimit && Contains(c.Config.Attachments.SupportedMimeTypes, req.Header.Get("Content-Type")) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func UploadMedia(GlobalConfig *GlobalConfig) {
+	client, config := GetMastodonClientWithLimits(GlobalConfig)
+	image := Fetch(GlobalConfig)
+	for !ValidateMedia(config, image.URL) {
+		image = Fetch(GlobalConfig)
+	}
+	req, err := http.Get(image.URL)
+	if err != nil {
+		log.Fatal("Unable to make GET request to endpoint ", image.URL, ": ", err)
+	}
+	body, _ := io.ReadAll(req.Body)
+	req.Body.Close()
+	attachment, err := client.UploadMediaFromBytes(context.Background(), body)
+	if err != nil {
+		log.Fatal("Unable to upload media to Mastodon: ", err)
+	}
+	status, err := client.PostStatus(context.Background(), &mastodon.Toot{
+		Status:      fmt.Sprintf("%s (by u/%s)", image.Title, image.Author),
+		InReplyToID: "",
+		MediaIDs:    []mastodon.ID{attachment.ID},
+		Sensitive:   false,
+		SpoilerText: "",
+		Visibility:  "",
+		Language:    "",
+	})
+	if err != nil {
+		log.Fatal("Unable to post status to Mastodon: ", err)
+	}
+	fmt.Printf(status.URL)
+}
